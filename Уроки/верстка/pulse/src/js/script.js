c93a68d4aa7f6105d871449e61b601ddf910c324
Leesheronyx <Leesheronyx@gmail.com>
2021-08-23 16:16:08 +0600

    up

---
+++ Уроки/верстка/pulse/src/js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+	$('.carousel__inner').slick({
+		speed: 1200,
+		adaptiveHeight: true,
+		autoplay: true,
+		prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.svg"></button>'
+	});
+});
